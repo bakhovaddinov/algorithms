4b2944c468729b5346cdfc08056a0051a6ffb1fe
Iskandar <32882346+bakhovaddinov@users.noreply.github.com>
2018-12-08 14:42:59 +0300

    Create sorting_the_list2.py

---
+++ sorting_the_list2.py
@@ -0,0 +1,12 @@
+massive = []
+N = int(input('Введите количество элементов массива '))
+for i in range(N):
+        massive.append(int(input('Введите элементы массива последовательно ')))
+
+for i in range(len(massive)):
+        if massive[i] < massive [i+1]:
+                massive[i], massive[i+1] = massive[i+1], massive[i]
+
+
+
+print(massive)
